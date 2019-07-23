c8b170ab1c4efdee0a0c3ee98bcbd3259fbbc90e
olyapetryshyn <olya.petryshyn@gmail.com>
2019-07-23 14:30:35 +0300

    modified resources and Dockerfile.

--- form_service/config/base_config.py
+++ form_service/config/base_config.py
@@ -1,10 +1,10 @@
 """Configuration for form service."""
 LOCAL_DATABASE = 'postgres://postgres:postgres@localhost:5432/4m_forms'
-PG_DOCKER_DATABASE = 'postgres://postgres:mysecretpassword@172.17.0.2:5432/4m_forms'
+PG_DOCKER_DATABASE = 'postgres://postgres:mysecretpassword@db:5432/4m_forms'
 
 
 class Configuration:
     """Base configuration class."""
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = LOCAL_DATABASE
+    SQLALCHEMY_DATABASE_URI = PG_DOCKER_DATABASE
     SQLALCHEMY_TRACK_MODIFICATIONS = True
